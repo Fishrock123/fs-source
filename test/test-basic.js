ebac18d014a04a074007b60dd552696ee1bd70eb
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2019-07-15 12:09:56 -0700

    use bob-streams' assertion-sink

--- test/test-basic.js
+++ test/test-basic.js
@@ -1,18 +1,20 @@
 'use strict'
 
 const FileSource = require('../index.js')
-const AssertionSink = require('./assertion-helper-sink')
+const { AssertionSink } = require('bob-streams')
 
 const fileSource = new FileSource('./test/fixtures/file')
 const assertionSink = new AssertionSink(
   [
     'Chunk one\ndata data\n\n',
     'Chunk two\ndata data\n\n',
-    'Chunk three\ndata data\n'
-  ]
+    'Chunk three\ndata data\n',
+    ''
+  ],
+  'utf8'
 )
 
-assertionSink.bindSource(fileSource, error => {
+assertionSink.bindSource(fileSource).start(error => {
   if (error)
     console.error('Stream returned ->', error.stack)
   else {
