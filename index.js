e1780d6cebd26e78204285aa2c3beaae7ccccd71
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2019-07-15 12:23:17 -0700

    add linting and use tap for tests

--- index.js
+++ index.js
@@ -17,28 +17,28 @@ class FileSource {
 
     this.path = path
 
-    this.fd = options.fd === undefined ? null : options.fd;
-    this.flags = options.flags === undefined ? 'r' : options.flags;
-    this.mode = options.mode === undefined ? 0o666 : options.mode;
+    this.fd = options.fd === undefined ? null : options.fd
+    this.flags = options.flags === undefined ? 'r' : options.flags
+    this.mode = options.mode === undefined ? 0o666 : options.mode
 
-    this.pos = 0;
+    this.pos = 0
 
     if (options.start !== undefined) {
       if (typeof options.start !== 'number') {
         throw new TypeError(`options.start MUST be a Number, found ${typeof options.start}`)
       }
       if (options.end === undefined) {
-        options.end = Infinity;
+        options.end = Infinity
       } else if (typeof options.end !== 'number') {
         throw new TypeError(`options.end MUST be a Number or undefined, found ${typeof options.end}`)
       }
 
       if (options.start > options.end) {
-        const errVal = `{start: ${options.start}, end: ${options.end}}`;
+        const errVal = `{start: ${options.start}, end: ${options.end}}`
         throw new RangeError(`start must be greater than end, found ${errVal}`)
       }
 
-      this.pos = options.start !== undefined ? options.start : 0;
+      this.pos = options.start !== undefined ? options.start : 0
     }
 
     // XXX(Fishrock123):
@@ -96,7 +96,7 @@ class FileSource {
         })
       } else {
         if (bytesRead > 0) {
-          this.pos += bytesRead;
+          this.pos += bytesRead
           this.sink.next(status.continue, null, buffer, bytesRead)
         } else {
           fs.close(this.fd, (closeError) => {
@@ -117,9 +117,8 @@ module.exports = FileSource
 
 /* ## Helpers ## */
 
-function copyObject(source) {
-  var target = {};
-  for (var key in source)
-    target[key] = source[key];
-  return target;
+function copyObject (source) {
+  var target = {}
+  for (var key in source) { target[key] = source[key] }
+  return target
 }
--- test/test-basic.js
+++ test/test-basic.js
@@ -1,23 +1,25 @@
 'use strict'
 
+const tap = require('tap')
+
 const FileSource = require('../index.js')
 const { AssertionSink } = require('bob-streams')
 
-const fileSource = new FileSource('./test/fixtures/file')
-const assertionSink = new AssertionSink(
-  [
-    'Chunk one\ndata data\n\n',
-    'Chunk two\ndata data\n\n',
-    'Chunk three\ndata data\n',
-    ''
-  ],
-  'utf8'
-)
+tap.test('test file read', t => {
+  t.plan(1)
+  const fileSource = new FileSource('./test/fixtures/file')
+  const sink = new AssertionSink(
+    [
+      'Chunk one\ndata data\n\n',
+      'Chunk two\ndata data\n\n',
+      'Chunk three\ndata data\n',
+      ''
+    ],
+    'utf8'
+  )
 
-assertionSink.bindSource(fileSource).start(error => {
-  if (error)
-    console.error('Stream returned ->', error.stack)
-  else {
-    console.log('ok')
-  }
+  sink.bindSource(fileSource).start(error => {
+    t.error(error, 'Exit Callback received unexpected error')
+    t.end()
+  })
 })
